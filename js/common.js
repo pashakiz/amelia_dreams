2af5c27be8e596f4a231109c9e70e964e9af2783
pashakiz <pashakiz@gmail.com>
2019-01-08 04:50:37 +0300

    save.html done

---
+++ js/common.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $('.header').on('click', '.header__btn', function(){
+        var header = $(this).closest('.header');
+        var btn = $(this).closest('.header').find('.header__btn');
+        var menu = $(this).closest('.header').find('.header__menu');
+        if ( btn.hasClass('icon-menu-down') ) {
+            menu.slideUp();
+            btn.removeClass('icon-menu-down');
+            btn.addClass('icon-menu');
+        } else {
+            menu.slideDown();
+            btn.removeClass('icon-menu');
+            btn.addClass('icon-menu-down');
+        }
+    });
+
+});
